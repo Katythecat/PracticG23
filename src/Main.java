e4d951e13bcd4f1fd24cdaf14a35abf0fb4d8b9c
ramidayoub0791 <121964405+ramidayoub0791@users.noreply.github.com>
2023-04-05 10:32:32 -0400

    Rami

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our first project");
+        System.out.println("Hello I am Rami");
+        System.out.println("I did my changes");
     }
 }
