cd7f43914f5860aa001fe5aff5ac634a90aba8bc
Katythecat <kk.wilasinee@gmail.com>
2023-04-05 07:46:28 -0700

    cat feature added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our first project");
+
+
+
+        cat c=new cat();
+        c.name="Leo";
+        c.age="8";
+        c.price="3000";
     }
 }
