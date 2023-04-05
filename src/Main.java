0bb0d87ebbe896346afda7c251fd13b8005ac392
natagusina <natagusina@gmail.com>
2023-04-05 15:45:38 +0100

    I added the branch Nata

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our first project");
+
+        Nata nata=new Nata();
+        nata.features="high";
+        nata.project="first";
     }
 }
